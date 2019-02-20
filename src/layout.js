8a18e863c916dddb40b56ce5f5b81a8556ac4521
CHEN <chengf_28@163.com>
2019-02-20 15:14:03 +0800

    优化显示样式,颜色调整,配置调整;

--- src/layout.js
+++ src/layout.js
@@ -1,99 +1,95 @@
 var layout = {
-	__style:{
-		overflow:{
-			background       : 'rgba(0,0,0,0.6)',
-			width            : '100%',
-			height           : '100%',
-			position         : 'fixed',
-			top              : 0,
-			left             : 0,
-			right            : 0,
-			bottom           : 0,
+	__style: {
+		overflow: {
+			'background'     : 'rgba(0,0,0,0.6)',
+			'width'          : '100%',
+			'height'         : '100%',
+			'position'       : 'fixed',
+			'top'            : 0,
+			'left'           : 0,
+			'right'          : 0,
+			'bottom'         : 0,
 			"z-index"        : 10000,
-			display          : 'flex',
+			'display'        : 'flex',
 			'align-items'    : 'center',
 			'justify-content': 'center'
 		},
-		box:{
-			position       : 'relative',
-			background     : '#fff',
-			color          : 'black',
-			"border-radius": '.5rem',
-			overflow       : 'hidden',
-			transition     : 'all .4s',
-			transform      : 'scale(0)'
+		box: {
+			'position'  : 'relative',
+			'color'     : 'black',
+			'overflow'  : 'hidden',
+			'transition': 'all .4s',
+			'transform' : 'scale(0)'
 		},
-		box_head:{
-			background: '#336666',
-			padding   : '1rem 0 1rem 1rem',
-			color     : '#fff'
+		box_head: {
+			'background'             : '#00c292',
+			'padding'                : '1rem 0 1rem 1rem',
+			'color'                  : '#fff',
+			'border-top-left-radius' : '.5rem',
+			'border-top-right-radius': '.5rem'
 		},
 		box_body: {
-			"max-width": '300px',
-			"word-wrap": 'break-word',
-			padding    : '2rem 3rem'
+			'max-width' : '300px',
+			'word-wrap' : 'break-word',
+			'padding'   : '2rem 3rem',
+			'background': '#fff',
 		},
 		box_foot: {
-			padding          : '0 1rem 1rem 1rem',
-			display          : 'flex',
-			"justify-content": 'flex-end',
-			width            : '100%',
-			"box-sizing"     : 'border-box'
+			'padding'                   : '0 1rem 1rem 1rem',
+			'display'                   : 'flex',
+			'justify-content'           : 'flex-end',
+			'width'                     : '100%',
+			'box-sizing'                : 'border-box',
+			'background'                : '#fff',
+			'border-bottom-left-radius' : '.5rem',
+			'border-bottom-right-radius': '.5rem'
 		},
-		box_foot_button:{
-			display        : ' block',
-			background     : ' none',
-			outline        : ' none',
-			border         : ' none',
-			padding        : ' .5rem',
-			cursor         : ' pointer',
-			margin         : ' 0 .5rem',
-			border         : ' 1px solid #ddd',
-			"border-radius": ' .5rem',
-			transition:'all .3s'
+		box_foot_button: {
+			'display'      : ' block',
+			'background'   : ' none',
+			'outline'      : ' none',
+			'border'       : ' none',
+			'padding'      : ' .5rem',
+			'cursor'       : ' pointer',
+			'margin'       : ' 0 .5rem',
+			'border'       : ' 1px solid #ddd',
+			'border-radius': ' .5rem',
+			'transition'   : 'all .3s'
 		},
 	},
-	__toole:{
-		style:function(style)
-		{
+	__toole: {
+		style: function (style) {
 			var css = '';
-			for( var key in style )
-			{
-				css += key+':'+style[key]+';';
+			for (var key in style) {
+				css += key + ':' + style[key] + ';';
 			}
 			return css;
 		}
 	},
-	__index:[],
-	overflow:function(){
-		var div = this.cElement('',this.__style.overflow,div,id='layout-overflow')
+	__index: [],
+	overflow: function () {
+		var div = this.cElement('', this.__style.overflow, div, id = 'layout-overflow')
 		// document.body.appendChild(div);
 		return div;
 	},
-	cElement: function (text = '',style = '',type = 'div',id='')
-	{
+	cElement: function (text = '', style = '', type = 'div', id = '') {
 		var dom = document.createElement(type);
-		if (style)
-		{
+		if (style) {
 			dom.style.cssText = this.__toole.style(style);
 		}
-		if (text.length>0) 
-		{
+		if (text.length > 0) {
 			dom.innerText = text;
 		}
-		if (id.length>0) 
-		{
+		if (id.length > 0) {
 			dom.id = id;
 		}
 		return dom;
 	},
-	appendNode:function (node)
-	{
+	appendNode: function (node) {
 
 		node.forEach(n => {
 			// 有子节点,向下探
-			if (n.hasOwnProperty('nodeChild')) 
-			{
+			if (n.hasOwnProperty('nodeChild')) {
 				// 将父级节点堆入堆中;
 				this.__index.push(n.nodeRoot);
 				// 继续探索
@@ -101,113 +97,99 @@ var layout = {
 				this.__index.pop();
 			}
 			// 如果没有子节点,进行dom操作,在上级dom中,执行操作
-			var dom = this.__index[this.__index.length-1];
-			if (dom) 
-			{
+			var dom = this.__index[this.__index.length - 1];
+			if (dom) {
 				dom.appendChild(n.nodeRoot);
-			}else{
+			} else {
 				document.body.appendChild(n.nodeRoot);
 			}
-			
+
 		})
-		
+
 	},
 	// 消息方法
-	alert:function(msg,config,...func)
-	{
-		var title      = '消息';
+	alert: function (msg, config, ...func) {
+		var title = '消息';
 		var head_style = this.__style.box_head;
-		var btn_name   = ['确认'];
+		var btn_name = ['确认'];
 		// 如果是对象,则判断
-		if (config)
-		{
+		if (config) {
 			// 对象
-			if (typeof (config) === 'object' && isNaN(config.length)) 
-			{
-				title   = !config.hasOwnProperty('title') ? title: config['title'];
-				if (config.hasOwnProperty('btn')) 
-				{
-					switch (typeof(config['btn']))
-					{
+			if (typeof (config) === 'object' && isNaN(config.length)) {
+				title = !config.hasOwnProperty('title') ? title : config['title'];
+				if (config.hasOwnProperty('btn')) {
+					switch (typeof (config['btn'])) {
 						case 'string':
 							btn_name[0] = config['btn'];
 							break;
-					
+
 						case 'object':
 							btn_name = config['btn'];
 							break;
 					}
 				}
-				for(key in this.__style.box_head)
-				{
-					if (msg.hasOwnProperty(key)) 
-					{
+				for (key in this.__style.box_head) {
+					if (msg.hasOwnProperty(key)) {
 						head_style[key] = msg[key];
 					}
 				}
 			}
 			// 如果没有填写配置,直接执行function,将其堆入func中
-			if (typeof(config) === 'function') 
-			{
-				if (func) 
-				{
+			if (typeof (config) === 'function') {
+				if (func) {
 					func.unshift(config);
-				}else{
+				} else {
 					func = [config];
 				}
 			}
 		}
-		
+
 
 
 		// 添加遮罩层
-		var partent   = this.overflow();
+		var partent = this.overflow();
 		/* 生成dom元素 */
 		// 生成元素
 		var btn_array = [];
-		for (let index = 0; index < btn_name.length; index++) 
-		{
-			btn_array.push({nodeRoot:this.cElement(btn_name[index],this.__style.box_foot_button,'button','button'+index)});
+		for (let index = 0; index < btn_name.length; index++) {
+			btn_array.push({ nodeRoot: this.cElement(btn_name[index], this.__style.box_foot_button, 'button', 'button' + index) });
 		}
-		var b_foot    = this.cElement('', this.__style.box_foot, 'div');
-		var b_head    = this.cElement(title,head_style, 'div');
-		var b_body    = this.cElement(msg, this.__style.box_body, 'div');
-		var box       = this.cElement('',this.__style.box,'div','layout-box');
+		var b_foot = this.cElement('', this.__style.box_foot, 'div');
+		var b_head = this.cElement(title, head_style, 'div');
+		var b_body = this.cElement(msg, this.__style.box_body, 'div');
+		var box = this.cElement('', this.__style.box, 'div', 'layout-box');
 
 		// 元素添加节点
 		var node = [{
 			nodeRoot: partent,
 			nodeChild:
-			[{
-				nodeRoot: box,
-				nodeChild:[{
-					nodeRoot: b_head
-				},{
-					nodeRoot: b_body,
-				},{
-					nodeRoot: b_foot,
-					nodeChild:btn_array
+				[{
+					nodeRoot: box,
+					nodeChild: [{
+						nodeRoot: b_head
+					}, {
+						nodeRoot: b_body,
+					}, {
+						nodeRoot: b_foot,
+						nodeChild: btn_array
+					}]
 				}]
-			}]
 		}];
 		this.appendNode(node);
 		// 添加按钮事件监听
-		for (let i = 0; i < btn_array.length; i++) 
-		{
-			btn_array[i].nodeRoot.onmouseover = function(){
+		for (let i = 0; i < btn_array.length; i++) {
+			btn_array[i].nodeRoot.onmouseover = function () {
 				this.style.background = head_style['background'];
-				this.style.color      = '#fff';
+				this.style.color = '#fff';
 			};
 			btn_array[i].nodeRoot.onmouseout = function () {
 				this.style.background = 'none';
-				this.style.color      = '#000';
+				this.style.color = '#000';
 			};
 
 			// 添加点击关闭弹出层功能
-			btn_array[i].nodeRoot.onclick = function () 
-			{ 
-				if(func[i])
-				{
+			btn_array[i].nodeRoot.onclick = function () {
+				if (func[i]) {
 					func[i]();
 				}
 				layout.close();
@@ -216,12 +198,12 @@ var layout = {
 		}
 
 		// 延迟.001秒执行放大效果
-		setTimeout(function(){
+		setTimeout(function () {
 			box.style.transform = 'scale(1)';
-		},10);
+		}, 10);
 
 	},
-	close:function (){
+	close: function () {
 		document.body.removeChild(document.getElementById('layout-overflow'));
 	}
 }
